e080acb2d2fbe3b0d5264e9c0836f34340f14292
annacallan <annacallan@gmail.com>
2019-10-19 12:37:28 +0800

    Reorganising the file structure and moving app to repo

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CurrentWeather from './components/CurrentWeather';
-
-function App() {
-  return <CurrentWeather city="Perth" temperature="34" />;
-}
-
-export default App;
